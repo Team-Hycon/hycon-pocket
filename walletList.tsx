c6b177cd24c5290c6d91e3b473e00fdfb93756c6
unknown <cindy@glosfer.com>
2019-01-08 07:19:01 +0900

    rate us work need to be improved (how often prompt ....)

--- walletList.tsx
+++ walletList.tsx
@@ -26,10 +26,10 @@ import { Link } from "react-router-dom"
 import { IRest } from "../rest"
 import Onboarding from "./content/onboarding"
 import { IText } from "./locales/m_locales"
+
 // tslint:disable:no-var-requires
 const logo = require("./img/logo.png")
 const storage = window.localStorage
-
 // tslint:disable:object-literal-sort-keys
 const styles = (theme: Theme) => createStyles({
     root: {
@@ -64,6 +64,51 @@ class WalletList extends React.Component<IProps, any> {
     constructor(props: IProps) {
         super(props)
 
+
+        AppRate.preferences = {
+            displayAppName: 'Hycon Pocket',
+            usesUntilPrompt: 2,
+            promptAgainForEachNewVersion: true,
+            inAppReview: true,
+            useLanguage: "en",
+            simpleMode: true,
+            storeAppURL: {
+                ios: 'io.hycon.litewallet',
+                android: 'market://details?id=io.hycon.litewallet',
+                windows: 'ms-windows-store://pdp/?ProductId=<the apps Store ID>',
+                blackberry: 'appworld://content/[App Id]/',
+                windows8: 'ms-windows-store:Review?name=<the Package Family Name of the application>'
+            },
+            customLocale: {
+                title: "Would you mind rating %@?",
+                message: "It won’t take more than a minute and helps to promote our app. Thanks for your support!",
+                cancelButtonLabel: "No, Thanks",
+                laterButtonLabel: "Remind Me Later",
+                rateButtonLabel: "Rate It Now",
+                yesButtonLabel: "Yes!",
+                noButtonLabel: "Not really",
+                appRatePromptTitle: 'Do you like using %@',
+                appRatePromptMessage: 'If you like using this wallet please give us your feedback',
+                feedbackPromptTitle: 'Mind giving us some feedback?',
+                feedbackPromptMessage: 'Choose the option you prefer for giving us a feedback',
+            },
+            callbacks: {
+                handleNegativeFeedback: function () {
+                    window.open('mailto:feedback@example.com', '_system');
+                },
+                onRateDialogShow: function (callback) {
+                    callback(1) // cause immediate click on 'Rate Now' button
+                },
+                onButtonClicked: function (buttonIndex) {
+                    console.log("onButtonClicked -> " + buttonIndex);
+                },
+                done: function () {
+                    console.log("feedback done");
+                }
+            }
+        };
+        AppRate.promptForRating(false);
+
         this.state = {
             rest: this.props.rest,
             language: this.props.language,
@@ -84,6 +129,8 @@ class WalletList extends React.Component<IProps, any> {
         this.state.rest.getFavoriteList().then((data: any) => {
             this.setState({ contactList: data })
         })
+
+
     }
 
     public componentDidMount() {
